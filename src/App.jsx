d808a2cbdc63544884e7708ed2711f84a0223444
Mary Martinez <mary.diana.martinez@gmail.com>
2022-04-26 17:40:15 -0700

    followed along in class as long as I could keep up, then just focused on paying attention :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { asyncGetQuotes, thenGetQuotes, tryGetQuotes, tryThenGetQuotes } from './services/promise-me';
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+    <button onClick={async() => console.log(await asyncGetQuotes())}>async Get Quotes</button>
+    <button onClick={async() => console.log(await thenGetQuotes())}>then Get Quotes</button>
+    <button onClick={async() => console.log(await tryGetQuotes())}>try Get Quotes</button>
+    <button onClick={async() => console.log(await tryThenGetQuotes())}>tryThen Get Quotes</button>
+    </>
+  );
 }
